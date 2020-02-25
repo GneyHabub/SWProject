1db15d83158c87be6827163bb3b8d8382e852af9
GneyHabub <31561808+GneyHabub@users.noreply.github.com>
2020-02-25 13:08:19 +0300

    Minor changes

--- frontend/index.js
+++ frontend/index.js
@@ -1,16 +1,16 @@
-let flag = false;
+let login_switch_flag = false;
 
 document.getElementById("switch").addEventListener("click", function () {
-    if (!flag) {
-        document.getElementById("poll_id").style.display = "inline";
+    if (!login_switch_flag) {
+        document.getElementById("survey_id").style.display = "inline";
         document.getElementById("switch").innerText = "Login as a staff member";
         document.getElementById("access_header_text").innerText = "Student Access";
-        flag = true;
+        login_switch_flag = true;
     }else {
-        document.getElementById("poll_id").style.display = "none";
+        document.getElementById("survey_id").style.display = "none";
         document.getElementById("switch").innerText = "Login as a student";
         document.getElementById("access_header_text").innerText = "University Staff Access";
-        flag = false;
+        login_switch_flag = false;
     }
 
 });
