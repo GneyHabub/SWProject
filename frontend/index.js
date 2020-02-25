4ee1ef13a332b78d792a5d4e191a524398496b63
GneyHabub <31561808+GneyHabub@users.noreply.github.com>
2020-02-25 11:16:28 +0300

    Finished login page

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,16 @@
-new Vue({
-    el: '#inp_box',
-    data() {
-        return {
-            password: ''
-        }
+let flag = false;
+
+document.getElementById("switch").addEventListener("click", function () {
+    if (!flag) {
+        document.getElementById("poll_id").style.display = "inline";
+        document.getElementById("switch").innerText = "Login as a staff member";
+        document.getElementById("access_header_text").innerText = "Student Access";
+        flag = true;
+    }else {
+        document.getElementById("poll_id").style.display = "none";
+        document.getElementById("switch").innerText = "Login as a student";
+        document.getElementById("access_header_text").innerText = "University Staff Access";
+        flag = false;
     }
+
 });
