914554cb7f97b605d7ec4d76b46d05ae777055ca
BulbaWarrior <vladkalm49@gmail.com>
2020-03-27 13:35:57 +0300

    migration to remote postgesql database, so now everyone's data is consistent

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'assessment',
+        'USER': 'postgres',
+        'PASSWORD': '4682',
+        'HOST': '10.90.136.59',
+        'PORT': '5432',
     }
 }
 
