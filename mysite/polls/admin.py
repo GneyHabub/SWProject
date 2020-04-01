c5d90c519393fac8ea14f5592a0a48971067eebb
pollytur <havegreatsuccessq@gmail.com>
2020-04-01 21:26:34 +0300

    added users, login, redirections after login and tests for users

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,40 +1,80 @@
 from django.contrib import admin
-
-# Register your models here.
 from django.contrib import admin
+import nested_admin
 
-from .models import Question, Choice, Poll
+from .models import Question, Choice, Poll, CustomUser
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
+    extra = 0
+    sortable_options = "id"
 
 
-class QuestionInline(admin.TabularInline):
+class QuestionInline(nested_admin.NestedStackedInline):
     model = Question
     extra = 0
+    sortable_option = "id"
     inlines = [ChoiceInline]
 
 
-class PollAdmin(admin.ModelAdmin):
+class PollAdmin(nested_admin.NestedModelAdmin):
     fieldsets = [
         (None, {'fields': ['poll_name']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date']}),
+        ('Available dates', {'fields': ['open_date', 'close_date']}),
     ]
     inlines = [QuestionInline]
-    list_display = ('poll_name', 'pub_date')
+    list_display = ('poll_name', 'pub_date', 'open_date', 'close_date')
     list_filter = ['pub_date']
-    # search_fields = ['question_text']
 
 
-class QuestionAdmin(admin.StackedInline):
+class QuestionAdmin(nested_admin.NestedStackedInline):
     fieldsets = [
         (None, {'fields': ['question_text', 'type']})]
     inlines =[ChoiceInline]
     list_display = ('question_text')
     model = Question
     extra = 7
+
+# class StudentAdmin(nested_admin.NestedStackedInline):
+#     fieldsets = [
+#         (None, {'fields': ['question_text', 'type']})]
+#     list_display = ('question_text')
+#     model = Student
+#     extra = 0
+
+# class UserAdmin(nested_admin.NestedModelAdmin):
+#     fieldsets = [
+#         ('User info', {'fields': ['name', 'email', 'password', 'role']})]
+#     list_display = ('name', 'email', 'password', 'role')
+#     model = User
+#     extra = 0
+
+#     todo maybe it should be changed to Nested User Admin
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'is_doe', 'is_student', 'is_prof', 'is_active',)
+    list_filter = ('email', 'is_doe', 'is_student', 'is_prof', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_doe', 'is_student', 'is_prof', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_doe', 'is_student', 'is_prof', 'is_active', 'is_staff')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 # class QuestionAdmin(admin.ModelAdmin):
     # fieldsets = [
@@ -52,9 +92,9 @@ class QuestionAdmin(admin.StackedInline):
 # admin.site.register(Choice)
 
 
+# admin.site.register(User, UserAdmin)
 admin.site.register(Poll, PollAdmin)
 
 admin.site.register(Question)
-
 admin.site.register(Choice)
 
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
@@ -8,6 +9,11 @@ urlpatterns = [
 # ex: /polls/
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    url('thank_you_page/', views.thank_you, name='thank_you_page'),
     path('<int:poll_id>/vote/', views.vote_poll, name='vote'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    url(r'^login_staff/$', views.staff_login, name='login_staff'),
+    url(r'^login_student/$', views.student_login, name='login_student'),
+    path('<int:pk>/user', views.PersonalPage.as_view(), name='personal_page')
 ]
