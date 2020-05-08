976f6c7df508faec0e88ac3c825c226e3e3b6c58
GneyHabub <31561808+GneyHabub@users.noreply.github.com>
2020-05-08 18:24:21 +0300

    connected courses list to API

--- mysite/polls/static/courses_list/courses_list_script.js
+++ mysite/polls/static/courses_list/courses_list_script.js
@@ -2,16 +2,12 @@ Vue.component('course_info', {
     template: `
         <div class="course_info">
             <h2 @click="load">{{course_name}}</h2>
-            <p @click="load">{{course_description}}</p>
         </div>`,
     props: {
         course_name:{
             type: "String",
             required: true,
             default: "Course Name"
-        },
-        course_description:{
-            type: "String"
         }
     },
     methods: {
@@ -26,26 +22,7 @@ new Vue({
     el: "#app",
     data: function(){ 
         return{
-            semesters: [
-                {
-                    name: "Spring 2020",
-                    value: "spring20"
-                },
-                {
-                    name:"Fall 2019",
-                    value: "fall19"
-                },
-                {
-                    name: "Spring 2019",
-                    value: "spring19"
-                },
-                {
-                    name: "Fall 2018",
-                    value: "fall18"
-                }
-            ],
-            selected_semester:"Semester",
-
+            courses: [],
             profiles: [
                 {
                     name: "Math",
@@ -73,82 +50,14 @@ new Vue({
                 }
             ],
             selected_degree: "Degree",
-
-            course_descriptions: [
-                {
-                    name: "[S20] Probability and Statistics",
-                    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec felis vel leo pellentesque porttitor. Nam a urna eu urna mattis luctus. Nullam dapibus suscipit velit, sed tempor lectus auctor ac. Vivamus sit amet orci at augue rutrum fermentum vitae interdum massa. Praesent in mauris quis sem fringilla consequat. Sed sed neque at sem pharetra volutpat.",
-                    semester: "spring20",
-                    profile: "math",
-                    degree: "bachelor"
-                },
-                {
-                    name: "[F18] Computer Architecture",
-                    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec felis vel leo pellentesque porttitor. Nam a urna eu urna mattis luctus. Nullam dapibus suscipit velit, sed tempor lectus auctor ac. Vivamus sit amet orci at augue rutrum fermentum vitae interdum massa. Praesent in mauris quis sem fringilla consequat. Sed sed neque at sem pharetra volutpat.",
-                    semester: "fall18",
-                    profile: "cs",
-                    degree: "bachelor"
-                },
-                {
-                    name: "[S19] English for Academic Purposes II",
-                    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec felis vel leo pellentesque porttitor. Nam a urna eu urna mattis luctus. Nullam dapibus suscipit velit, sed tempor lectus auctor ac. Vivamus sit amet orci at augue rutrum fermentum vitae interdum massa. Praesent in mauris quis sem fringilla consequat. Sed sed neque at sem pharetra volutpat.",
-                    semester: "spring19",
-                    profile: "english",
-                    degree: "bachelor"
-                },
-                {
-                    name: "[S19] Analytical Geometry and Linear Algebra II",
-                    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec felis vel leo pellentesque porttitor. Nam a urna eu urna mattis luctus. Nullam dapibus suscipit velit, sed tempor lectus auctor ac. Vivamus sit amet orci at augue rutrum fermentum vitae interdum massa. Praesent in mauris quis sem fringilla consequat. Sed sed neque at sem pharetra volutpat.",
-                    semester: "spring19",
-                    profile: "math",
-                    degree: "bachelor"
-                },
-                {
-                    name: "[F19] Operating Systems",
-                    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec felis vel leo pellentesque porttitor. Nam a urna eu urna mattis luctus. Nullam dapibus suscipit velit, sed tempor lectus auctor ac. Vivamus sit amet orci at augue rutrum fermentum vitae interdum massa. Praesent in mauris quis sem fringilla consequat. Sed sed neque at sem pharetra volutpat.",
-                    semester: "fall19",
-                    profile: "cs",
-                    degree: "bachelor"
-                },
-                {
-                    name: "[S20] Control Theory (Linear Control)",
-                    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec felis vel leo pellentesque porttitor. Nam a urna eu urna mattis luctus. Nullam dapibus suscipit velit, sed tempor lectus auctor ac. Vivamus sit amet orci at augue rutrum fermentum vitae interdum massa. Praesent in mauris quis sem fringilla consequat. Sed sed neque at sem pharetra volutpat.",
-                    semester: "spring20",
-                    profile: "math",
-                    degree: "bachelor"
-                },
-                {
-                    name: "[F18] Introduction to Programming I",
-                    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec felis vel leo pellentesque porttitor. Nam a urna eu urna mattis luctus. Nullam dapibus suscipit velit, sed tempor lectus auctor ac. Vivamus sit amet orci at augue rutrum fermentum vitae interdum massa. Praesent in mauris quis sem fringilla consequat. Sed sed neque at sem pharetra volutpat.",
-                    semester: "fall18",
-                    profile: "cs",
-                    degree: "bachelor"
-                },
-                {
-                    name: "[F19] Physics I (mechanics)",
-                    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec felis vel leo pellentesque porttitor. Nam a urna eu urna mattis luctus. Nullam dapibus suscipit velit, sed tempor lectus auctor ac. Vivamus sit amet orci at augue rutrum fermentum vitae interdum massa. Praesent in mauris quis sem fringilla consequat. Sed sed neque at sem pharetra volutpat.",
-                    semester: "fall19",
-                    profile: "math",
-                    degree: "bachelor"
-                }
-            ],
             filter_query: ""
         } 
     },
     computed: {
         filtered_course_descriptions() {
-            let res = this.course_descriptions;
-            if (this.selected_semester != "Semester"){
-                res =  res.filter(course => course.semester === this.selected_semester);
-            }
-            if(this.selected_profile != "Profile"){
-                res =  res.filter(course => course.profile === this.selected_profile);
-            }
-            if(this.selected_degree != "Degree"){
-                res =  res.filter(course => course.degree === this.selected_degree);
-            }
+            let res = this.courses;
             let filter = new RegExp(this.filter_query, "i");
-            res = res.filter(course => course.name.match(filter));
+            res = res.filter(course => course.match(filter));
             return res;
         }
     },
@@ -159,5 +68,13 @@ new Vue({
             this.selected_degree = "Degree";
             this.filter_query = "";
         }
+    },
+    created() {
+        const url = window.location.href.slice(0, -1) + "_api/";
+        fetch(url).then(res => {
+            return res.json();
+        }).then(res => {
+            this.courses = res["COURSES"];
+        })
     }
 });
